ca159bfbb1e254f927547b50617f8fb11271e2a4
tajwararik <tajwar.arik.10@gmail.com>
2025-12-07 16:44:29 +0600

    Created createUser function

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,9 +1,32 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "../lib/prisma";
+
 export function getIndexPage(req, res) {
   res.render("index");
 }
 
 export function getSignUpForm(req, res) {
   res.render("signup");
+}
+
+export async function createUser(req, res) {
+  const { firstName, lastName, email, password } = req.validatedUser;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+    res.render("signup");
+  }
 }
 
 export function getLogInForm(req, res) {
