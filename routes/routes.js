51c3ec417cc4a9aea7e20d0291faff91369712c5
tajwararik <tajwar.arik.10@gmail.com>
2025-12-07 16:47:19 +0600

    Implemented post method to create user

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,16 @@ import { Router } from "express";
 import {
   getIndexPage,
   getSignUpForm,
+  createUser,
   getLogInForm,
 } from "../controllers/controllers.js";
+import { userCreatePost } from "../validator/userValidator.js";
 
 const router = Router();
 
 router.get("/", getIndexPage);
 router.get("/signup", getSignUpForm);
+router.post("/signup", userCreatePost, createUser);
 router.get("/login", getLogInForm);
 
 export default router;
