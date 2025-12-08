47c845d628067d552781558e4d136d6e950767c9
tajwararik <tajwar.arik.10@gmail.com>
2025-12-08 19:37:42 +0600

    Added routes to log in and log out

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,11 @@ import {
   getSignUpForm,
   createUser,
   getLogInForm,
+  getHomePage,
+  userLogOut,
 } from "../controllers/controllers.js";
 import { userCreatePost } from "../validator/userValidator.js";
+import passport from "../validator/passport.js";
 
 const router = Router();
 
@@ -13,5 +16,14 @@ router.get("/", getIndexPage);
 router.get("/signup", getSignUpForm);
 router.post("/signup", userCreatePost, createUser);
 router.get("/login", getLogInForm);
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  })
+);
+router.get("/home", getHomePage);
+router.get("/logout", userLogOut);
 
 export default router;
